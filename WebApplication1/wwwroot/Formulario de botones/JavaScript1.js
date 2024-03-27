d7e6a44144647cc644bdeea62c1334492964279a
Alumno <Alumno@DESKTOP-K95NRN5>
2024-03-27 14:11:33 +0100

    Agregar archivos de proyecto.

---
+++ WebApplication1/wwwroot/Formulario de botones/JavaScript1.js
@@ -0,0 +1,20 @@
+﻿var numero = document.getElementById("valor");
+var aumentar = document.getElementById("aumentar");
+var reducir = document.getElementById("reducir");
+
+var rojo = document.getElementById("rojo");
+var verde = document.getElementById("verde");
+var azul = document.getElementById("azul");
+
+function randomColor() {
+    var color = '#';
+    var colorCode = ['ffaf40', '2A9BFB', 'ff5967', '7870cc', '33ccbf']; // colors
+    var className = document.getElementsByClassName("change"); // class name to random color
+    var i;
+    color += colorCode[Math.floor(Math.random() * colorCode.length)];
+    for (var i = 0; i < className.length; i++) {
+        className[i].style.backgroundColor = color;
+    }
+}
+
+
